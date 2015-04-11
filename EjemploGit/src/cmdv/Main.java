d97d89c20ceb3427667ad85c7d32a545f7e3a2e2
Chris <crimarde@gmail.com>
2015-04-11 07:47:35 +0200

    Commit inicial del ejemplo de Git

---
+++ EjemploGit/src/cmdv/Main.java
@@ -0,0 +1,13 @@
+package cmdv;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Main main = new Main();
+		main.saluda();
+	}
+	
+	public void saluda(){
+		System.out.println("Hola mundo!");
+	}
+}
